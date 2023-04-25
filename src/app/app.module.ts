e115388dc2ec3208fc661e7e66588a104ec53eac
katlp48 <katylezameta@gmail.com>
2023-04-25 00:11:15 -0500

    books

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,11 @@ import {MatInputModule} from '@angular/material/input';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import {MatButtonModule} from '@angular/material/button';
+import { BookComponent } from './component/book/book.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule,
